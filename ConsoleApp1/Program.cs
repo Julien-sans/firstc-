4616dfbeb41bc095ee6acabacafb05087aae8f71
Julien S <julien1sans@gmail.com>
2019-11-17 14:52:05 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class A
+{
+    int entier = 42;
+    public int Entier
+    {
+        get
+        {
+            return entier;
+        }
+        set
+        {
+            entier = value > 0 ? value : 0;
+        }
+    }
+    private void MaMethodePrivee()
+    {
+        Console.WriteLine("OLEOLEOLE");
+    }
+    public void MaMethode()
+    {
+        Console.WriteLine("ABCD");
+        MaMethodePrivee();
+    }
+}
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Modification(A a)
+        {
+            a.Entier = 10;
+        }
+        static void Main(string[] args)
+        {
+            var test = new A();
+
+            test.Entier = -5;
+            Console.WriteLine(test.Entier);
+            test.MaMethode();
+        }
+    }
+}
